06ab78403bc0619c7a46e4fadf7e5d5882ff7d29
SDZZGNDRC <sdzzgndrc@gmail.com>
2024-08-05 14:30:10 +0800

    backup

--- Chapter1/S2C_Dimension.lean
+++ Chapter1/S2C_Dimension.lean
@@ -9,10 +9,14 @@ import Mathlib.Data.Real.Basic
 import Mathlib.Data.Fin.VecNotation
 import Mathlib.RingTheory.MvPolynomial.Basic
 
--- TODO) 2.35 Basis length does not depend on basis
+-- 2.35 Basis length does not depend on basis
 /-
 Any two bases of a finite-dimensional vector space have the same length.
 -/
+
+-- dimension theorem
+#check mk_eq_mk_of_basis
+
 
 -- 2.36 Definition: Dimension, dim V
 
@@ -55,21 +59,85 @@ If V is finite-dimensional and U is a subspace of V, then dim U ≤ dim V.
 Suppose V is finite-dimensional. Then every linearly independent list of
 vectors in V with length dim V is a basis of V.
 -/
+-- may help:
+#check linearIndependent_le_basis
+
 
 -- TODO) 2.40 example: Show that the list (5,7),(4,3) is a basis of F².
-def ex_2_40 : Basis (Fin 2) F (Fin 2 → F) := {
-  repr := sorry
+noncomputable def ex_2_40 : Basis (Fin 2) F (Fin 2 → F) := Basis.ofEquivFun {
+    toFun := fun v => fun x => match x with
+      | 0 => 7 / 13 * (v 0) - 5 / 13 * (v 1)
+      | 1 => -(3 / 13) * (v 0) + 4 / 13 * (v 1)
+    map_add' := by sorry
+    map_smul' := sorry
+    invFun := sorry
+    left_inv := sorry
+    right_inv := sorry
+  }
+
+-- FIXME: Can we remove [LinearOrder F]
+-- Show that list (1,1), (1,-1) is a basis of F².
+noncomputable example : Basis (Fin 2) ℝ (Fin 2 → ℝ) := Basis.ofEquivFun {
+  toFun := fun v => fun x => match x with
+    | 0 => (v 0 + v 1) / 2
+    | 1 => (v 0 - v 1) / 2
+  map_add' := by
+    intro a b
+    ext x
+    fin_cases x
+    · simp_all
+      linarith
+    · simp_all
+      linarith
+  map_smul' := by
+    intro r a
+    ext x
+    fin_cases x
+    · simp_all
+      linarith
+    · simp_all
+      linarith
+  invFun := fun v => fun x => match x with
+    | 0 => v 0 + v 1
+    | 1 => v 0 - v 1
+  left_inv := by
+    simp_all
+    dsimp [Function.LeftInverse]
+    intro v
+    ext x
+    fin_cases x
+    · simp_all
+      linarith
+    · simp_all
+      linarith
+  right_inv := by
+    simp_all
+    dsimp [Function.RightInverse]
+    intro v
+    ext x
+    fin_cases x
+    · simp_all
+    · simp_all
 }
 
+-- may help:
+#check Basis.ofEquivFun
+#check Basis.finTwoProd
+#check LinearEquiv.finTwoArrow
+#check LinearEquiv
+#check LinearMap
+#check AddEquiv
+  #check Equiv
 
 -- TODO) 2.41 example
 
--- TODO) 2.42 Spanning list of the right length is a basis
+-- 2.42 Spanning list of the right length is a basis
 /-
 Suppose V is finite-dimensional. Then every spanning list of vectors in V
 with length dim V is a basis of V.
+TODO: Need to show that the rank is dim V.
 -/
-
+#check Basis.mk   -- A linear independent family of vectors spanning the whole module is a basis.
 
 -- 2.43 Dimension of a sum
 /-
